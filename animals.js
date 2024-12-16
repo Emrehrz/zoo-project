33064943d27cb04fade533446facc023e0200724
Emrehrz <horuzemre3@gmail.com>
2024-12-16 16:23:50 +0300

    fixing object creation, initializing animal objects

--- animals.js
+++ animals.js
@@ -4,6 +4,7 @@ class Hunter {
     this.locationX = locationX;
     this.locationY = locationY;
     this.huntedAnimals = [];
+    this.attackLength = attackLength
   }
 
   move(maxX, maxY) {
@@ -18,68 +19,88 @@ class Hunter {
 }
 
 class Animal {
-  constructor(id, locationX, locationY, gender = null) {
+  constructor(id, locationX, locationY, gender = null, hasGender = true, isHunted = false) {
     this.id = id;
     this.locationX = locationX;
     this.locationY = locationY;
-    this.gender = gender || (Math.random() < 0.5 ? 'male' : 'female');
+    this.gender = hasGender ? (gender || (Math.random() < 0.5 ? 'male' : 'female')) : null;
+    this.isHunted = isHunted;
+    this._speed = 0
   }
 
-  move(maxX, maxY) {
-    const dx = getRandomStep(this.constructor.speed);
-    const dy = getRandomStep(this.constructor.speed);
+  get speed() {
+    return this._speed
+  }
 
-    // Keep new coordinates within bounds
-    this.locationX = Math.max(0, Math.min(maxX, this.locationX + dx));
-    this.locationY = Math.max(0, Math.min(maxY, this.locationY + dy));
+  set speed(value) {
+    this._speed = value
+  }
+
+  move() {
+    const [dx, dy] = this.getRandomMove();
+
+    this.locationX = Math.max(0, Math.min(500, this.locationX + dx));
+    this.locationY = Math.max(0, Math.min(500, this.locationY + dy));
+  }
+
+  getRandomMove() {
+    const angle = Math.random() * 2 * Math.PI; // 0 ile 2*pi arasında bir açı
+
+    const dx = this.speed * Math.cos(angle);
+    const dy = this.speed * Math.sin(angle);
+
+    return [dx, dy];
   }
 }
 
-class Leon extends Animal {
-  static speed = 4;
 
-  constructor(id, locationX, locationY, ishunted = false, gender = null, attackLength = 5) {
-    super(id, locationX, locationY, gender);
-    this.ishunted = ishunted
+class Lion extends Animal {
+
+  constructor(id, locationX, locationY, gender = null, attackLength = 5) {
+    super(id, locationX, locationY, gender, true)
+    this.speed = 4
+    this.attackLength = attackLength
   }
 }
 
 class Wolf extends Animal {
-  static speed = 3;
-  constructor(id, locationX, locationY, ishunted = false, gender = null, attackLength = 4) {
-    super(id, locationX, locationY, gender);
-    this.ishunted = ishunted;
+  constructor(id, locationX, locationY, gender = null, attackLength = 4) {
+    super(id, locationX, locationY, gender, true);
+    this.speed = 3;
+    this.attackLength = attackLength
   }
 }
 
 class Sheep extends Animal {
   static speed = 2;
-  constructor(id, locationX, locationY, ishunted = false, gender = null) {
-    super(id, locationX, locationY, gender);
-    this.ishunted = ishunted;
+  constructor(id, locationX, locationY, gender = null) {
+    super(id, locationX, locationY, gender, true,);
+
   }
 }
 
 class Cow extends Animal {
-  static speed = 2;
-  constructor(id, locationX, locationY, ishunted = false, gender = null) {
-    super(id, locationX, locationY, gender);
-    this.ishunted = ishunted;
+  constructor(id, locationX, locationY, gender = null) {
+    super(id, locationX, locationY, gender, true);
+    this.speed = 2;
   }
 }
 
 class Chicken extends Animal {
-  static speed = 1;
   constructor(id, locationX, locationY) {
-    super(id, locationX, locationY, null);
+    super(id, locationX, locationY, null, false, false);
+    this.speed = 1; // Set speed via setter
   }
 }
+
 
 class Cock extends Animal {
   static speed = 1;
+
   constructor(id, locationX, locationY) {
-    super(id, locationX, locationY, null);
+    super(id, locationX, locationY, null, false);
   }
 }
 
-export { Leon, Wolf, Sheep, Cow, Chicken, Cock, Hunter };
+
+export { Lion, Wolf, Sheep, Cow, Chicken, Cock, Hunter };
--- main.js
+++ main.js
@@ -1,59 +1,62 @@
-import { Leon, Wolf, Sheep, Cow, Chicken, Cock, Hunter } from './animals.js';
+import { Hunter, Lion, Wolf, Sheep, Cow, Chicken, Cock } from "./animals.js"
 
 
-// TODO: yeterli mesafede ve avlanmamis hayvan mi kontrol yap
-function Hunting(hunter, hunt) {
+function initialRandomLocation() {
+  let x, y
+
+  x = Math.random() * 500
+  y = Math.random() * 500
+
+  return { x, y }
+
 }
 
-function getRandomLocation() {
-  const x = Math.random() * 500;
-  const y = Math.random() * 500;
-  return { x, y };
-}
+function createAnimals(animalClass, count, hasGender = true) {
+  const animals = [];
+  const halfCount = Math.floor(count / 2);  // Yarı sayıda erkek ve dişi olacak şekilde ayarlıyoruz
 
+  for (let i = 0; i < count; i++) {
+    const gender = hasGender ? (i < halfCount ? 'male' : 'female') : null;  // İlk yarısı erkek, geri kalanı dişi
+    const animal = new animalClass(i + 1, initialRandomLocation().x, initialRandomLocation().y, gender);
+    animals.push(animal);
+  }
+  return animals;
+}
 // baslangicta belirli sayida hayvanlari belirli cinsiyetlerde olustur. 
 function simulation() {
-  const sheeps = [];
-  const cows = [];
-  const wolves = [];
-  const lions = [];
-  const chickens = [];
-  const cocks = [];
-  const hunter = new Hunter(1, Math.random() * 100, Math.random() * 100);
+  const sheeps = createAnimals(Sheep, 30);  // 30 koyun
+  const cows = createAnimals(Cow, 10);     // 10 inek
+  const wolves = createAnimals(Wolf, 10);  // 10 kurt
+  const lions = createAnimals(Lion, 8);    // 8 aslan
+  const chickens = createAnimals(Chicken, 10, false);  // 10 tavuk (cinsiyet yok)
+  const cocks = createAnimals(Cock, 10, false);        // 10 horoz (cinsiyet yok)
 
-  // Create 30 sheep (15 male, 15 female)
-  for (let i = 0; i < 30; i++) {
-    const gender = i < 15 ? 'male' : 'female';
-    sheeps.push(new Sheep(i + 2, Math.random() * 100, Math.random() * 100, false, gender));
-  }
+  const hunter = new Hunter(1, initialRandomLocation().x, initialRandomLocation().y);
 
-  // Create 10 cows (5 male, 5 female)
-  for (let i = 0; i < 10; i++) {
-    const gender = i < 5 ? 'male' : 'female';
-    cows.push(new Cow(i + 32, Math.random() * 100, Math.random() * 100, false, gender));
-  }
+  // 30 sheeps
 
-  // Create 10 wolves (5 male, 5 female)
-  for (let i = 0; i < 10; i++) {
-    const gender = i < 5 ? 'male' : 'female';
-    wolves.push(new Wolf(i + 42, Math.random() * 100, Math.random() * 100, false, gender));
-  }
+  // 10 cows
 
-  // Create 8 lions (4 male, 4 female)
-  for (let i = 0; i < 8; i++) {
-    const gender = i < 4 ? 'male' : 'female';
-    lions.push(new Leon(i + 52, Math.random() * 100, Math.random() * 100, false, gender));
-  }
+  // 10 chickens
 
-  // Create 10 chickens
-  for (let i = 0; i < 10; i++) {
-    chickens.push(new Chicken(i + 60, Math.random() * 100, Math.random() * 100));
-  }
+  // 10 wolfs
 
-  // Create 10 cocks
-  for (let i = 0; i < 10; i++) {
-    cocks.push(new Cock(i + 70, Math.random() * 100, Math.random() * 100));
-  }
+  // 10 cocks
 
-  // bin birimlik  simulasyonu gerceklestir
+  // 8 lions
+
+  console.log(lions);
+
+
+
+  // for (let step = 0; step < 1000; step++) {
+  //   hunter.move()
+
+  // }
+
 }
+
+
+simulation()
+
+
